e543a3b7c90c20829fe42f09ffbaef4899991183
pubudu <pubz87@gmail.com>
2014-02-16 14:29:44 +0530

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wearmytags::Application.config.secret_key_base = '046178a69c0668ec0357ec7d7246a7e6a38e508af345d83362002b800a2b1e36622d337bc822162e36eb2dede3b2fd8c6411cdd3023e8a99ec46a19d2948faed'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wearmytags::Application.config.session_store :cookie_store, key: '_wearmytags_session'
