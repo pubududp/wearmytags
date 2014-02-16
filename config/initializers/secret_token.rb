444df3e183f94268efdd7e166a7e2d2bce3bf73b
pubudu <pubz87@gmail.com>
2014-02-16 15:51:55 +0530

    add rspec

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
 Wearmytags::Application.config.secret_key_base = '046178a69c0668ec0357ec7d7246a7e6a38e508af345d83362002b800a2b1e36622d337bc822162e36eb2dede3b2fd8c6411cdd3023e8a99ec46a19d2948faed'
